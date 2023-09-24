aff79308fbe652ad34774108c3e83e56acf23410
u7770276 <u7770276@anu.edu.au>
2023-09-24 12:17:07 +1000

    Zexin TANG InPairTest AngleTest RugTest ScoresTest.java

---
+++ tests/comp1110/ass2/testdata/AngleTest.java
@@ -0,0 +1,59 @@
+package comp1110.ass2.testdata;
+
+import comp1110.ass2.Angle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AngleTest {
+
+    @Test
+    public void testGetAngleFromValue() {
+        // Description: Enumeration from value test perspective.
+        // This test case evaluates the getAngleFromValue method of the angle enumeration.
+        // This test case evaluates the getAngleFromValue method of the angle enumeration. // It verifies that the enumeration correctly associates an angle value with the corresponding enumeration value in the range 0 to 359 degrees.
+
+        // Scenario: Call the getAngleFromValue method with various angle values in the range 0 to 359 degrees.
+
+        // Expected result: The method should return the correct angle enumeration value for each angle value in the valid range.
+
+        // Assertion verifying that the method correctly maps angle values to angle enumeration values:
+
+        assertEquals(Angle.DEG_0, Angle.getAngleFromValue(0)); // Verifies that 0 degrees corresponds to Angle.DEG_0.
+        assertEquals(Angle.DEG_90, Angle.getAngleFromValue(90)); // Ensures that 90 degrees corresponds to Angle.DEG_90.
+        // Similar assertions for other angles within the valid range.
+    }
+
+    @Test
+    public void testAdd() {
+        // Description: Testing angle addition
+        // This test case evaluates the add method of the angle enumeration, which adds two angle values and returns the corresponding enumeration value.
+
+        // Scenario
+        // 1. Create Angle enumeration values representing angles such as Angle.DEG_0, Angle.DEG_90, and Angle.DEG_180.
+        //DEG_0, Angle.DEG_90, and Angle.DEG_180. // Use the add method to add these enumerations.
+
+        // Expected result: The add method should correctly perform the angle addition and return the corresponding angle enumeration values.
+
+        // Assertions to verify the correctness of the angle addition:
+
+        assertEquals(Angle.DEG_90, Angle.DEG_0.add(Angle.DEG_90)); // Adding Angle.DEG_0 and Angle.DEG_90 should result in Angle.DEG_90.
+        // Similar assertions for other angle additions.
+    }
+
+    @Test
+    public void testGetRad() {
+        // Description: Tests the conversion of angles to radians.
+        // This test case examines the getRad method of the angle enumeration, which converts angle values to radians.
+
+        // Scenario
+        // 1. Create Angle enumeration values that represent angles, such as Angle.DEG_0, Angle.DEG_90, and Angle.DEG_180.
+        // DEG_0, Angle.DEG_90, and Angle.DEG_180. // 2. Use the getRad method to convert these enumerated values to radians.
+
+        // Expected result: The getRad method should correctly convert angle values to radians.
+
+        // Assertions to verify correct radian conversion:
+        assertEquals(Math.toRadians(0), Angle.DEG_0.getRad()); // Angle.DEG_0 should convert to 0 radians.
+        assertEquals(Math.toRadians(90), Angle.DEG_90.getRad()); // Angle.DEG_90 should convert to π/2 radians.
+        // Similar assertions for other angle conversions.
+    }
+}
---
+++ tests/comp1110/ass2/testdata/IntPairTest.java
@@ -0,0 +1,98 @@
+package comp1110.ass2.testdata;
+
+import comp1110.ass2.IntPair;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntPairTest {
+
+    @Test
+    public void testAdd() {
+        // Test the addition of IntPair objects
+        // Verify that the add method of the IntPair class correctly performs element-level summing and compares it to the expected value
+
+        // Scenario:
+        // 1. Initialise two IntPair objects:
+        // - pair1 with coordinates (5, 0).
+        // - pair2 has coordinates (0, 4).
+        // 2. Call the add method, passing pair2 as an argument to pair1.
+
+        // Desired result: the addition should result in an IntPair object with coordinates (5, 4).
+
+        // Use assertions to verify that the result is correct:
+        IntPair pair1 = new IntPair(5, 0);
+        IntPair pair2 = new IntPair(0, 4);
+        IntPair result = pair1.add(pair2);
+
+        assertEquals(5, result.getX()); // Verify that the X-coordinate of the result matches the expected value (5)
+        assertEquals(4, result.getY()); // Verify that the resultant Y coordinate matches the expected value (4)
+    }
+
+    @Test
+    public void testIsValidPosition() {
+        // Test the validity of the IntPair object
+        // Verify that the isValidPosition method of the IntPair class correctly determines if the coordinates are within the valid range of the 7x7 board
+
+        // Scenario:
+        // 1. Create a valid IntPair object with coordinates (3, 4).
+        // 2. Create an invalid IntPair object with coordinates (-1, 7).
+
+        // Expected result:
+        // - The valid IntPair object should be treated as valid.
+        // - An invalid IntPair object should be treated as invalid.
+
+        // Use assertions to verify the correctness of the method:
+        IntPair validPosition = new IntPair(3, 4);
+        IntPair invalidPosition = new IntPair(-1, 7);
+
+        assertTrue(validPosition.isValidPosition()); // Verify that a valid IntPair object is considered valid
+        assertFalse(invalidPosition.isValidPosition()); // Verify that invalid IntPair objects are treated as invalid
+    }
+
+    @Test
+    public void testIsAdjacentTo() {
+        // Test whether IntPair objects are adjacent to each other
+        // Verify that the isAdjacentTo method of the IntPair class correctly determines whether two coordinates are adjacent or not
+
+        // Scenario:
+        // 1. Create two adjacent IntPair objects with coordinates (3, 4) and (3, 5).
+        // 2. Create two non-adjacent IntPair objects with coordinates (2, 2) and (5, 6).
+
+        // Expected result:
+        // - Adjacent IntPair objects should be treated as adjacent.
+        // - Non-adjacent IntPair objects should be treated as non-adjacent.
+
+        // Use assertions to verify the correctness of the methods:
+        IntPair adjacent1 = new IntPair(3, 4);
+        IntPair adjacent2 = new IntPair(3, 5);
+        IntPair notAdjacent1 = new IntPair(2, 2);
+        IntPair notAdjacent2 = new IntPair(5, 6);
+
+        assertTrue(adjacent1.isAdjacentTo(adjacent2)); // Verify that adjacent IntPair objects are treated as neighbouring
+        assertFalse(adjacent1.isAdjacentTo(notAdjacent1)); // Verify that non-adjacent IntPair objects are treated as non-adjacent
+        assertFalse(adjacent1.isAdjacentTo(notAdjacent2)); // Verify that non-adjacent IntPair objects are treated as non-adjacent
+    }
+
+    @Test
+    public void testCalculateVectorTo() {
+        // Test calculating a vector from one IntPair to another IntPair
+        // Verify that the calculateVectorTo method of the IntPair class correctly calculates a vector from one coordinate to another
+
+        // Scenario:
+        // 1. Create two IntPair objects with coordinates (3, 4) and (6, 2).
+
+        // Expected results:
+        // - Calculate that the vector from (3, 4) to (6, 2) should be (3, -2).
+
+        // Use assertions to verify the correctness of the method:
+        IntPair from = new IntPair(3, 4);
+        IntPair to = new IntPair(6, 2);
+        IntPair vector = from.calculateVectorTo(to);
+
+        assertEquals(3, vector.getX()); // Verify that the X coordinate is correct
+        assertEquals(-2, vector.getY()); // Verify that the Y coordinate is correct
+    }
+}
+
+
